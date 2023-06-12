570e668f3098b138c5d13d90678be6fa35410542
Saurabh jasiwal <62091292+SaurabhxJaiswal@users.noreply.github.com>
2023-06-12 21:25:08 +0530

    Container With Most Water

---
+++ Container With Most Water/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        int i = 0;
+        int j = height.Length - 1;
+        int MaxArea = 0; 
+        while(i < j)
+        {
+            var Area = Math.Min(height[i], height[j]) * (j - i);
+            MaxArea = Math.Max(MaxArea, Area);
+            if(height[i] < height[j])
+            {
+                i++; 
+            }
+            else
+            {
+                j--;
+            }
+        }
+
+        return MaxArea; 
+    }
+}
