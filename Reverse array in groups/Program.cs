4e242384d313bd643e230eb052321886f715629d
Saurabh jasiwal <62091292+SaurabhxJaiswal@users.noreply.github.com>
2023-06-12 21:58:49 +0530

    Reverse array in groups

---
+++ Reverse array in groups/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+class Solution
+{
+    //Complete this function
+    public void reverseInGroups(int[] A, int N, int K)
+    {
+        var result = new int[N];
+        for (int i = 0; i < N + N % K; i++)
+        {
+            var part1 = K - (K % i);
+            var part2 = K*(Math.Floor((double)i/K));
+
+            var final = part1 + part2;
+            if(final< N)
+            {
+                result[i] = A[(int)final]; 
+            }
+        }
+        A = result;
+    }
+
+}
